d81d90442903833e5c62bc1c9dcafd1bda24f1c7
dahee <daheewa128@gmail.com>
2022-01-17 00:39:59 +0900

    add Controller, SecurityConfig

---
+++ src/main/java/com/study/prospring5/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.study.prospring5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/","/home").permitAll()
+            .and().authorizeRequests().antMatchers("/singers/**").authenticated();
+
+        http.formLogin().loginPage("/login").permitAll();
+        http.logout().permitAll();//.logoutSuccessUrl(성공시 갈 url)
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()//스프링시큐리티가 실행될 때 사용자를 생성하고, 애플리케이션 종료시 삭제
+                .withUser("user").password("user").roles("USER");
+    }
+}
---
+++ src/main/java/com/study/prospring5/web/SingerController.java
@@ -0,0 +1,58 @@
+package com.study.prospring5.web;
+
+import com.study.prospring5.domain.Singer;
+import com.study.prospring5.service.SingerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping(value="/singers")
+public class SingerController {
+    private final Logger logger = LoggerFactory.getLogger(SingerController.class);
+
+    @Autowired
+    SingerService singerService;
+
+    @GetMapping
+    public String list(Model model){
+        logger.info("Listing singers");
+        List<Singer> singers = singerService.findAll();
+        model.addAttribute("singers", singers);
+        logger.info("No. of singers: "+singers.size());
+        return "singers";
+    }
+
+    @GetMapping(value = "/{id}")
+    public String show(@PathVariable("id") Long id, Model model){
+        Singer singer = singerService.findById(id);
+        model.addAttribute("singer",singer);
+        return "show";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String updateForm(@PathVariable Long id, Model model){
+        model.addAttribute("singer", singerService.findById(id));
+        return "update";
+    }
+
+    @GetMapping(value = "/new")
+    public String createForm(Model model){
+        Singer singer = new Singer();
+        model.addAttribute("singer",singer);
+        return "update";
+    }
+
+    @PostMapping
+    public String saveSinger(@Valid Singer singer){
+        singerService.save(singer);
+        return "redirect:/singers/"+singer.getId();
+    }
+
+}
